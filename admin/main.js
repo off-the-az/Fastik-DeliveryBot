c8c63f7189f4f369a04cc8e0a351cbdfe0687f4c
off-the-az <0ff.th3.az@gmail.com>
2023-03-01 15:24:51 +0200

    Minor fixes

--- admin/main.js
+++ admin/main.js
@@ -1,14 +1,16 @@
 const { Telegraf } =  require("telegraf");
 require('dotenv').config()
 
-const {Auth} = require('./src/api/controller/index');
+const {User} = require('./src/api/controller/index');
 const {courier_menu_btn, admin_menu_btn} = require('./src/models/buttons');
 const readButtonCommands = require('./src/commands/buttonCommands');
 const bot = new Telegraf(process.env.TOKEN);
 
 
 bot.start(async (ctx) => {
-    let lvl = await Auth.login(ctx.chat.id);
+    let Users = new User();
+    let user = await Users.getByUsername(ctx.chat.id);
+    let lvl = user.user_lvl;
     lvl === 1 ? 
         await ctx.reply(`Вітаю, ${ctx.chat.first_name}🤗\nЛаскаво просимо вас в Кур'єр-Панель системи 'Fastik'. Оберіть пункт з меню через який ви бажаєте працювати із системою😊`, {reply_markup: courier_menu_btn}) : 
         lvl === 2 ? 
