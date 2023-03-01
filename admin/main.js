85a88b31418d9c7266f5ad6d1df24b0a443554e6
off-the-az <0ff.th3.az@gmail.com>
2023-03-01 13:52:40 +0200

    Adding new buttons for admins and users

--- admin/main.js
+++ admin/main.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const {Auth} = require('./src/api/controller/index');
 const {courier_menu_btn, admin_menu_btn} = require('./src/models/buttons');
 const readButtonCommands = require('./src/commands/buttonCommands');
+const readActionCommands = require('./src/commands/actionCommands');
 const bot = new Telegraf(process.env.TOKEN);
 
 
@@ -13,9 +14,18 @@ bot.start(async (ctx) => {
         await ctx.reply(`Вітаю, ${ctx.chat.first_name}🤗\nЛаскаво просимо вас в Кур'єр-Панель системи 'Fastik'. Оберіть пункт з меню через який ви бажаєте працювати із системою😊`, {reply_markup: courier_menu_btn}) : 
         lvl === 2 ? 
             await ctx.reply(`Вітаю, ${ctx.chat.first_name}🤗\nЛаскаво просимо вас в Адмін-Панель системи 'Fastik'. Оберіть пункт з меню через який ви бажаєте працювати із системою😊`, {reply_markup: admin_menu_btn}) : 
-                await ctx.reply('Упс...😕\nСхоже ви не маєте прав доступу до системи. Якщо вважаєте, що сталась помилка - зверніться до керівництва😉')
+                await ctx.reply('Упс...😕\nСхоже ви не маєте прав доступу до системи. Якщо бажаєте стати частинкою нашої команди натискай "Хочу в команду 🙋" і передай необхідні дані адміністрації 😉', {
+                    reply_markup:{
+                        keyboard:[
+                            ["Хочу в команду 🙋"],
+                        ],
+                        resize_keyboard: true, 
+                        is_persistent: true
+                    }
+                })
 });
 
 readButtonCommands(bot);
+readActionCommands(bot);
 
 bot.launch();
