63592307eac2cedf86ed0ff6b6c326ed7eb09c73
off-the-az <0ff.th3.az@gmail.com>
2023-03-01 15:50:20 +0200

    Minor fixes

--- admin/src/commands/buttonCommands.js
+++ admin/src/commands/buttonCommands.js
@@ -32,25 +32,19 @@ function readButtonCommands(bot){
     bot.action(/set_admin_(.+)/, async ctx => {
         const [id] = await ctx.match.slice(1);
         let Users = new User();
-        let user = await Users.getByUsername(id);
-        user.user_lvl = 2;
-        await Users.updateUser(id, user);
+        await Users.updateUser(id, {user_lvl: 2});
         await ctx.reply(`Права доступу користувача змінено на 'Адміністратор' ✅`);
     });
     bot.action(/set_courier_(.+)/, async ctx => {
         const [id] = await ctx.match.slice(1);
         let Users = new User();
-        let user = await Users.getByUsername(id);
-        user.user_lvl = 1;
-        await Users.updateUser(id, user);
+        await Users.updateUser(id, {user_lvl: 1});
         await ctx.reply(`Права доступу користувача змінено на 'Курʼєр' ✅`);
     });
     bot.action(/fired_(.+)/, async ctx => {
         const [id] = await ctx.match.slice(1);
         let Users = new User();
-        let user = await Users.getByUsername(id);
-        user.user_lvl = 0;
-        await Users.updateUser(id, user);
+        await Users.updateUser(id, {user_lvl: 0});
         await ctx.reply(`Права доступу користувача змінено на 'Користувач' ✅`);
     });
     bot.action(/finish_booking_(.+)/, async (ctx) => {
