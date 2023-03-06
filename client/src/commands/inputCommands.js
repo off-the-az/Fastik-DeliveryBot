01aea1cf3e330af2c365a53116071faf3e0bfc6d
0ff-the-az <0ff.th3.az@gmail.com>
2023-03-06 14:51:09 +0200

    Minor updates

--- client/src/commands/inputCommands.js
+++ client/src/commands/inputCommands.js
@@ -1,16 +1,15 @@
 const {User, Ticket} = require('../api/controller/index');
 const {menu_btn} = require('../models/buttons');
+const cmdList = require('../models/cmd.list.json');
 
 async function getAdress(bot){
     bot.hears(/(.+)/, async ctx => {
         const [adress] = ctx.match.slice(1);
         let controller = new User();
-        if(String(adress) != 'Скасувати ❌'){
+        if(String(adress) != cmdList.buttons.map(button => button.name)){
             await controller.updateUser(ctx.chat.id, {adress: String(adress)});
             let user = await controller.getByUsername(ctx.chat.id);
             await ctx.reply('Інформацію оновлено успішно✅\n' + `Натисни ще раз 'Оформити замовлення 📝' аби завершити оформлення замолення😉`, {reply_markup:menu_btn})
-        }else if(String(adress) === 'Скасувати ❌'){
-            await ctx.reply('Скасовано 😕❌\nЯкщо бажаєш внести адресу перед оформленням замовлення - натисни "Вказати адресу 🔄"😉\nУ разі якщо хочеш відразу вказати адрес під час оформлення замовлення - натисни "Оформити замовлення 📝" і вкажи адресу під час замовлення доставки😉', {reply_markup: menu_btn});
         }
     })
 }
