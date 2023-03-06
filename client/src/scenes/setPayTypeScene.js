201e850245ef0bc149723e36b7cf160156720d43
0ff-the-az <0ff.th3.az@gmail.com>
2023-03-06 16:13:36 +0200

    Update pay Method. Minor fixes

--- client/src/scenes/setPayTypeScene.js
+++ client/src/scenes/setPayTypeScene.js
@@ -1,17 +1,19 @@
 const { Scenes } =  require("telegraf");
 const {menu_btn} = require('../models/buttons');
 const {User} = require('../api/controller/index');
+const paymethod = require('../../../MenuDB/paymethod.json')
 
 const setPayTypeScene = new Scenes.BaseScene('setpaymethod');
-let type = "adress";
 
 setPayTypeScene.enter(async ctx => {
     await ctx.reply('Обери спосіб оплати, через який ти будеш розраховуватись за замовлення. Усі варіанти вказано в нижній панелі під полем де ти вносиш повідомлення', {reply_markup:{
-        keyboard: [
-            ['Оплата готівкою'],
-            ['Оплата картою'],
-            ['Передоплата'],
-            ['Скасувати ❌']
+        inline_keyboard: [
+            [
+                {text: 'Оплатити зараз', callback_data: 'now'}
+            ],
+            [
+                {text: 'Оплата кур’єру', callback_data: 'later'}
+            ]
         ],
         resize_keyboard: true,
     }});
@@ -20,29 +22,19 @@ setPayTypeScene.enter(async ctx => {
 setPayTypeScene.hears(/(.+)/, async ctx => {
     const [paymethod] = ctx.match.slice(1);
     let controller = new User();
-    if(String(paymethod) != 'Скасувати ❌'){
-        type = 'adress'
-        await controller.updateUser(ctx.chat.id, {payMethod: String(paymethod)});
+    if(String(paymethod) === 'now'){
+        await controller.updateUser(ctx.chat.id, {payMethod: 'Оплатити зараз'});
+        await ctx.reply('Для того щоби оплатити зараз дане замовлення потрібно зробити переказ на карту за даним реквізитом - ' + Number(paymethod.card_number));
         await ctx.scene.leave('setNumber');
-    }else if(String(paymethod) === 'Скасувати ❌'){
-        type = 'cancel';
+    }else if(String(paymethod) === 'later'){
+        await controller.updateUser(ctx.chat.id, {payMethod: 'Оплата кур’єру'});
+        await ctx.reply('Замовлення успішно оформлено! Очікуй інформації про замовлення!)');
         await ctx.scene.leave('setNumber');
     }
 })
 
 setPayTypeScene.leave(async ctx => {
-    type === 'adress' ?
-        await ctx.reply('Інформацію оновлено успішно✅\n' + `Натисни ще раз 'Продовжити ▶️' аби продовжити оформлення замолення😉`, {
-            reply_markup: {
-                inline_keyboard:[
-                    [
-                        
-                        {text: "Продовжити ▶️", callback_data: "finish_order"}
-                    ],
-                ]
-            }
-        })
-        : await ctx.reply('Скасовано 😕❌\nЯкщо бажаєш внести спосіб оплати перед оформленням замовлення - натисни "Вказати адресу 🔄"😉\nУ разі якщо хочеш відразу вказати адрес під час оформлення замовлення - натисни "Оформити замовлення 📝" і вкажи адресу під час замовлення доставки😉', {reply_markup: menu_btn});
+    console.log('Leave');
 })
 
 module.exports = setPayTypeScene;
