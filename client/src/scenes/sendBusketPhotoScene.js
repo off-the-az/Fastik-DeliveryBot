3344e4ded003f6c7ef9c12e672cc9e93cb04c457
0ff-the-az <0ff.th3.az@gmail.com>
2023-03-07 21:33:32 +0200

    Minor fixes

--- client/src/scenes/sendBusketPhotoScene.js
+++ client/src/scenes/sendBusketPhotoScene.js
@@ -5,7 +5,7 @@ const {User} = require('../api/controller/index');
 const {menu_btn} = require('../models/buttons');
 
 const sendBusketPhotoScene = new Scenes.BaseScene('sendBusketPhoto');
-const bot_sender = '6072101802:AAFnz6QLR4YssmqLeVMaRSAy5oA5bPd4AkU';
+const bot_sender = '5986688122:AAGfiCiyNIX_2shqSolWn-LtC0owxobDPAw';
 
 sendBusketPhotoScene.enter(async ctx => {
     await ctx.reply('Надішли нам фото/скрін де вказано, що саме ти хочеш у нас замовити і ми з часом звʼяжемось задля уточнень 😉');
@@ -15,16 +15,14 @@ sendBusketPhotoScene.on('photo', async ctx => {
     let Users = new User();
     let user = await Users.getByUsername(String(ctx.chat.id));
     const photo = ctx.message.photo[ctx.message.photo.length - 1];
-    const caption = `Ім'я: ${user.client_name}\nНомер телефону: +${user.pnumber}`;
+    const caption = `Прийгло нове замовлення\nІм'я: ${user.client_name}\nНомер телефону: +${user.pnumber}`;
     const form = new FormData();
-    form.append('chat_id', 5612131198); // FIXME: Need to change chat_id to Admin chat_id
+    form.append('chat_id', 	-1001819835850);
     form.append('photo', photo.file_id);
     console.info(photo.file_id);
     form.append('caption', caption);
-    await axios.post(`https://api.telegram.org/bot${bot_sender}/sendPhoto`, {
-        chat_id: 5612131198,
-        photo: photo.file_id,
-        caption: caption
+    await axios.post(`https://api.telegram.org/bot${bot_sender}/sendPhoto`, form, {
+        headers: form.getHeaders()
     }).then(async data => {
         await ctx.reply('Фото із кошиком успішно відправлено✅\nОчікуй на дзвіночок від менеджера 😉');
     }).catch(async (err) => {
