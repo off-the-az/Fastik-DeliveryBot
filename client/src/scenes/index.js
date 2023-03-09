539c79ce58e47079301b475653f42ce60c0ed92c
0ff-the-az <0ff.th3.az@gmail.com>
2023-03-09 02:26:11 +0200

    Minor fixes and updates

--- client/src/scenes/index.js
+++ client/src/scenes/index.js
@@ -1,11 +1,12 @@
 const { Scenes } =  require("telegraf");
+const setAddressScene = require('./setAdress');
+const nameScene = require('./setNameScene');
 const pnumberScene = require('./pnumberScene');
 const initBasketScene = require('./initBasketScene');
-const nameScene = require('./setNameScene');
 const setPayTypeScene = require('./setPayTypeScene');
 const setCommentaryScene = require('./setCommentaryScene');
 const sendBusketPhotoScene = require('./sendBusketPhotoScene');
 
-let stage = new Scenes.Stage([initBasketScene, pnumberScene, nameScene, setPayTypeScene, setCommentaryScene, sendBusketPhotoScene]);
+let stage = new Scenes.Stage([initBasketScene, pnumberScene, nameScene, setPayTypeScene, setCommentaryScene, sendBusketPhotoScene, setAddressScene]);
 
 module.exports = stage;
